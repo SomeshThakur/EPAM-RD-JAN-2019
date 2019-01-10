f2eb570a043253ec6e352603e54a3e66c97c7b21
Somesh Thakur <somesh_thakur@epam.com>
2019-01-10 12:09:51 +0530

    update immutable class with private modifier to data fields

--- WEEK-1/DAY-2/TASK-3/Planet.java
+++ WEEK-1/DAY-2/TASK-3/Planet.java
@@ -1,10 +1,10 @@
 import java.util.List;
 
 public final class Planet {
-	protected final List<Ocean> oceansList;
-	protected final List<Continent> continentsList;
-	protected final String name;
-	protected final double mass;
+	private final List<Ocean> oceansList;
+	private final List<Continent> continentsList;
+	private final String name;
+	private final double mass;
 
 	public Planet(final String name, final double mass, final List<Ocean> oceansList,
 			final List<Continent> continentsList) {
