03b3cf038c36c547ec3d516756d811e5cec46fec
Somesh Thakur <somesh_thakur@epam.com>
2019-01-11 17:38:22 +0530

    add Week-2/Day-1/Task-1

---
+++ WEEK-2/DAY-1/TASK-1/README.md
@@ -0,0 +1,4 @@
+# Task: To Write a program that prints the working directory to the console. 
+#### Output:
+
+![Output](https://i.imgur.com/RINB4le.png)
---
+++ WEEK-2/DAY-1/TASK-1/WorkingDir.java
@@ -0,0 +1,6 @@
+public class WorkingDir{
+	public static void main(String args[]){
+		final String dir = System.getProperty("user.dir");
+        System.out.println(dir);
+    }
+}
