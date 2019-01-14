28ebe64382102f018054141aa572ef16b67c2c2e
Somesh Thakur <somesh_thakur@epam.com>
2019-01-14 16:36:10 +0530

    fix Glitch

--- WEEK-1/DAY-2/TASK-1/StudentManagement.java
+++ WEEK-1/DAY-2/TASK-1/StudentManagement.java
@@ -4,7 +4,7 @@ import Task1.Student;
 public final class StudentManagement{
 	private final List<Student> students;
 
-	public StudentManagement(final List<Student> students){\
+	public StudentManagement(final List<Student> students){
 		/*Even if the list is final its modifiable, so we use Collection.unmodifiable to acheive immutablity*/
 		this.students=Collections.unmodifiableList(students);
 	}
